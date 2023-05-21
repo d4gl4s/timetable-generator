5b1c41fc4640482c0ddddd7e233348429b5377bd
jkirikal <johan@vhk.ee>
2023-05-21 13:23:06 +0300

    python file creates json with relevant courses information

--- api/test.py
+++ api/test.py
@@ -12,6 +12,28 @@ from urllib.request import urlopen
 import json
 import os
 import sys
+import copy
+
+def lisaTund(tabel, keskmine, ajavahemikud, tund):
+    for vahemik in ajavahemikud:
+        if keskmine > vahemik[0] and keskmine < vahemik[1] and tabel[ajavahemikud[vahemik]][int(tund[2])-1]==0:
+            tabel[ajavahemikud[vahemik]][int(tund[2])-1] = tund
+            return True
+    
+    return False
+
+def lisaPikkTund(aeg1, aeg2, tabel, ajavahemikud, tund):
+    kordusi = (aeg2-aeg1)/2
+    algusaeg = aeg1
+    kõikLisatud = True
+    for kordus in range(int(kordusi)):
+        keskmine = (algusaeg+algusaeg+2)/2
+        print(keskmine)
+        algusaeg += 2
+        lisatud = lisaTund(tabel, keskmine, ajavahemikud, tund)
+        if not lisatud:
+            kõikLisatud = False
+    return kõikLisatud
 #-------------------------setup------------------------------
 
 #------------------------------------------------------------
@@ -21,26 +43,102 @@ firstPart = "https://ois2.ut.ee/api/timetable/courses/"
 with open(os.path.join(sys.path[0], "links.txt"), "r") as f:
     read = f.read().splitlines()
 
+ained = {}
 for rida in read:
     url = firstPart+rida
-    print(url)
+    #print(url)
     response = urlopen(url)
     data_json = json.loads(response.read())
     if not 'info' in data_json:
         continue
     if not 'events' in data_json:
         continue
-    
-    print(data_json['info']['course_code'])
-    print(data_json['info']['title']['et'])
-    print(data_json['info']['credits'])
+
+    groups = {}
+    if 'groups' in data_json:
+        for group in data_json['groups']:
+            groups[group] = data_json['groups'][group]
+    ainekood = data_json['info']['course_code']
+    aine = [ainekood, data_json['info']['title']['et'], data_json['info']['credits']]
     for event in data_json['events']:
-        if event['event_type']['code'] == "lecture" and (event['study_work_type']["code"]=="practice" or event['study_work_type']["code"]=="lecture" or event['study_work_type']["code"]=="seminar"):
-            mainInfo = event['event_type']['code']+"|"+event['study_work_type']["code"]+"|"+event['time']['weekday']["code"]+"|"+event['time']['begin_time']+"|"+event['time']['end_time']
-            for i in event['notes']:
-                mainInfo = mainInfo + "|"+event['notes'][i]
-            print(mainInfo)
-    print("\n")
+        tund = []
+        note = ""
+        if ("-" in event['time']['academic_weeks'] or event['time']['academic_weeks'].count(",")>3) and event['event_type']['code'] == "lecture" and (event['study_work_type']["code"]=="practice" or event['study_work_type']["code"]=="lecture" or event['study_work_type']["code"]=="seminar"):
+            tund.extend((event['study_work_type']["code"], event['time']['weekday']["code"], event['time']['begin_time'], event['time']['end_time']))
+            for n in event['notes']:
+                note = event['notes'][n]
+            if 'group_uuids' in event:
+                for n in event['group_uuids']:
+                    tund.append(groups[n])
+        if len(tund) > 0 and len(note) < 30:
+            aine.append(tund)
+
+    if len(aine) > 3:
+        ained[ainekood] = aine
+
+
+with open(os.path.join(sys.path[0], "data.json"), "w", encoding='utf-8') as outfile:
+    json.dump(ained, outfile)
+
+
+""" tabel = []
+for i in range(6):
+    tabel.append([0,0,0,0,0])
+
+ajavahemikud = {(8,10):0, (10,12):1,(12,14):2,(14,16):3,(16,18):4,(18,20):5}
+
+soovitud = ["MTAT.03.105",
+"LTAT.03.005",
+"MTAT.03.294",
+"LTAT.06.026"]
+
+sor = list(sorted([ained[soov] for soov in soovitud], key = len))
+for s in sor:
+    indx = 3
+    for indeks in range(3, len(s)):
+        tund = s[indx]
+        tund.insert(0,s[0])
+        if len(tund)==5 or tund[1]=='lecture':
+            print(tund)
+            aeg1 = int(tund[3].split(":")[0])
+            aeg2 = int(tund[4].split(":")[0])
+            lisatud = False
+            if aeg2-aeg1 >2:
+                lisatud = lisaPikkTund(aeg1, aeg2, tabel, ajavahemikud, tund)
+            else:
+                keskmine = (aeg1+aeg2)/2
+                lisatud = lisaTund(tabel, keskmine, ajavahemikud, tund)
+            if lisatud:
+                del(s[indx])
+                indx -=1
+        indx+=1
+
+print(sor)
+
+def rek(ained, indeks, tabel, ajavahemikud):
+    if indeks == len(ained):
+        for t in tabel:
+            print(t)
+        print("\n")
+    elif len(ained[indeks]>3):
+        for tund in ained[indeks]:
+            print(tund)
+            aeg1 = int(tund[3].split(":")[0])
+            aeg2 = int(tund[4].split(":")[0])
+            lisatud = False
+            uusTabel = copy.deepcopy(tabel)
+            if aeg2-aeg1 >2:
+                lisatud = lisaPikkTund(aeg1, aeg2, uusTabel, ajavahemikud, tund)
+            else:
+                keskmine = (aeg1+aeg2)/2
+                lisatud = lisaTund(tabel, keskmine, uusTabel, tund)
+            if lisatud:
+                rek(ained, indeks+1, uusTabel, ajavahemikud)
+
+
+
+
+rek(sor, 0,tabel,ajavahemikud)
 
 
 
@@ -48,13 +146,7 @@ for rida in read:
 
 
 
-
-
-
-
-
-
-
+ """
 """
 chromiumdriver = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
 driver = webdriver.Chrome(service=Service(chromiumdriver))
